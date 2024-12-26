9e964610fa3dcb4898c1e90666ea25d1f98436da
Gianmaria <gianmaria.bonici@gmail.com>
2024-12-26 10:48:39 +0100

    release build by default

--- build.bat
+++ build.bat
@@ -1,7 +1,14 @@
 @echo off
 
 setlocal
+
+:: edit this to change the name of the file to build
+set file_to_build=main
+
 set mode=%1
+if "%mode%"=="" (
+    set mode=release
+)
 
 if "%mode%"=="debug" (
     set optimization=/Od /Zi /MDd
@@ -14,15 +21,17 @@ if "%mode%"=="debug" (
     exit /b 1
 )
 
-rem echo Building in: %mode%
+echo Building in %mode%
 rem echo Options: %opt%
 rem echo Macros: %macro%
 rem exit /b 0
 
 set base=%~dp0
+@REM echo Base is: %base%
 
-if not exist build\%mode% mkdir build\%mode%
-pushd build\%mode%
+set build_dir=%base%\build\%mode%
+if not exist %build_dir% mkdir %build_dir%
+pushd %build_dir%
 
 cl /nologo /FC /std:c++20 ^
 /EHsc ^
@@ -31,8 +40,8 @@ cl /nologo /FC /std:c++20 ^
 %macro% ^
 /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" ^
 /I%base% ^
-%base%\main.cpp ^
-/link /MACHINE:X64 /SUBSYSTEM:CONSOLE /DEBUG:full /OUT:main.exe
+%base%\%file_to_build%.cpp ^
+/link /MACHINE:X64 /SUBSYSTEM:CONSOLE /DEBUG:full /OUT:%file_to_build%.exe
 
 popd
 endlocal
